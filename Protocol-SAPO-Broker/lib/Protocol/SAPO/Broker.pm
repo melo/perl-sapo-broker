8ca4aa1742c37b3afafb52d07ceb248de8623019
Pedro Melo <melo@simplicidade.org>
2008-07-26 15:40:05 +0100

    Initial Protocol::SAPO::Broker template
    Generated with module-starter.
    
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

---
+++ Protocol-SAPO-Broker/lib/Protocol/SAPO/Broker.pm
@@ -0,0 +1,107 @@
+package Protocol::SAPO::Broker;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Protocol::SAPO::Broker - The great new Protocol::SAPO::Broker!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Protocol::SAPO::Broker;
+
+    my $foo = Protocol::SAPO::Broker->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Pedro Melo, C<< <melo at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-protocol-sapo-broker at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Protocol-SAPO-Broker>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Protocol::SAPO::Broker
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Protocol-SAPO-Broker>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Protocol-SAPO-Broker>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Protocol-SAPO-Broker>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Protocol-SAPO-Broker>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2008 Pedro Melo, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+1; # End of Protocol::SAPO::Broker
