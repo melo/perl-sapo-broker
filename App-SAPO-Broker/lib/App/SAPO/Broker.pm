505ee0ffd12bca333e83de6294a194fd7cbcc87a
Pedro Melo <melo@simplicidade.org>
2008-09-03 08:17:40 +0100

    Added a base App::SAPO::Broker
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

---
+++ App-SAPO-Broker/lib/App/SAPO/Broker.pm
@@ -0,0 +1,87 @@
+package App::SAPO::Broker;
+
+use warnings;
+use strict;
+
+our $VERSION = '0.01';
+
+
+=head1 NAME
+
+App::SAPO::Broker - The great new App::SAPO::Broker!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use App::SAPO::Broker;
+
+    my $foo = App::SAPO::Broker->new();
+    ...
+
+
+=head1 AUTHOR
+
+Pedro Melo, C<< <melo at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-app-sapo-broker at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=App-SAPO-Broker>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc App::SAPO::Broker
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=App-SAPO-Broker>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/App-SAPO-Broker>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/App-SAPO-Broker>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/App-SAPO-Broker>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2008 Pedro Melo, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+1; # End of App::SAPO::Broker
