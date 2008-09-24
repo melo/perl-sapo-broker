84dd98db985f98a85c7185c3b86e6dea13214cac
Pedro Melo <melo@simplicidade.org>
2008-09-24 13:35:48 +0100

    Support running all test and prepare smolder report
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- devel/run_all_tests.sh
+++ devel/run_all_tests.sh
@@ -2,7 +2,9 @@
 
 export PERL5LIB=`pwd`/Protocol-SAPO-Broker/lib
 
-if [ -z "$TEST_USE_HTML_REPORT"] ; then
+if [ -n "$SMOLDER_REPORT" ] ; then
+  prove_cmd='prove -l -v --archive smoke.tar.gz'
+elif [ -n "$TEST_USE_HTML_REPORT" ] ; then
 	prove_cmd='prove -l -m -Q --formatter=TAP::Formatter::HTML > output.html ; open output.html'
 else
   prove_cmd='prove -l -v'
